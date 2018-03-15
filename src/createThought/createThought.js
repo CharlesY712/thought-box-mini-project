067950520e4c291f33adceb485c5b3559640e32d
Charles Yach <CharlesY712@gmail.com>
2018-03-15 10:05:57 -0600

    Updating createThought.js to match tests

--- src/createThought/createThought.js
+++ src/createThought/createThought.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class CreateThought extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
   render() {
-    return(
+    return (
       <div>CreateThought</div>
     );
   }
 }
 
-export default CreateThought
+export default CreateThought;
--- src/createThought/createThought.test.js
+++ src/createThought/createThought.test.js
@@ -5,34 +5,34 @@ import CreateThought from './createThought.js';
 
 describe('CreateThought', () => {
 
-  it.skip('should match the snapshot', () => {
+  it('should match the snapshot', () => {
     const wrapper = shallow(<CreateThought />);
-    expect(wrapper).toMatchSnapshot()
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it.skip('updates the state of the title field', () => {
+  it('updates the state of the title field', () => {
     const wrapper = mount(<CreateThought/>);
-    const mockEvent = { target: { value: 'abc', name: 'title' } }
+    const mockEvent = { target: { value: 'abc', name: 'title' } };
     const expectedState = {
       title: 'abc',
       body: ''
     };
-    wrapper.instance().handleChange(mockEvent)
+    wrapper.instance().handleChange(mockEvent);
     expect(wrapper.state()).toEqual(expectedState);
   });
 
-  it.skip('updates the state of the body field', () => {
+  it('updates the state of the body field', () => {
     const wrapper = mount(<CreateThought/>);
-    const mockEvent = { target: { value: 'abc', name: 'body' } }
+    const mockEvent = { target: { value: 'abc', name: 'body' } };
     const expectedState = {
       title: '',
       body: 'abc'
     };
-    wrapper.instance().handleChange(mockEvent)
+    wrapper.instance().handleChange(mockEvent);
     expect(wrapper.state()).toEqual(expectedState);
   });
 
-  it.skip('calls submitIdea prop function with the data from state as an argument, and input fields go back to empty strings', () => {
+  it('calls submitIdea prop function with the data from state as an argument, and input fields go back to empty strings', () => {
     const mockedSubmit = jest.fn();
     const wrapper = shallow(
       <CreateThought createThought={mockedSubmit} />
